767597d6b4b06a8dac10befc2d5c417ebcc274b3
Veer Sahani <vsahani@purdue.edu>
2022-10-31 21:20:41 -0400

    create tables

--- database.sql
+++ database.sql
@@ -2,27 +2,15 @@ CREATE TABLE Player (
     id int NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-);
-
-CREATE TABLE PlayerAttributes (
-    player_id int NOT NULL,
-    position VARCHAR(255) NOT NULL,
-    height_feet in NOT NULL,
-    height_inches int NOT NULL,
-    weight_pounds float NOT NULL,
     team_id int NOT NULL,
-
-    PRIMARY KEY (player_id),
-    FOREIGN KEY (player_id) REFERENCES Player(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (team_id) REFERENCES Team(team_id)
 );
 
 CREATE TABLE Team (
     team_id int NOT NULL,
-    abbr VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     conference VARCHAR(255) NOT NULL,
-    divison VARCHAR(255)  NOT NULL,
     team_name VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (team_id),
@@ -40,6 +28,52 @@ CREATE TABLE GameStats (
     PRIMARY KEY (game_id),
     FOREIGN KEY (home_id) REFERENCES Team(team_id),
     FOREIGN KEY (away_id) REFERENCES Team(team_id),
+);
+
+CREATE TABLE PlayerStats (
+    player_id int NOT NULL,
+    game_id int NOT NULL,
+    points int NOT NULL,
+    rebounds int NOT NULL,
+    assists int NOT NULL,
+    blocks int NOT NULL,
+    steals int NOT NULL,
+    turnovers int NOT NULL,
+    fg_pct float NOT NULL,
+    fg3_pct float NOT NULL,
+    ft_pct float NOT NULL,
+
+
+    PRIMARY KEY (player_id),
+    FOREIGN KEY (player_id) REFERENCES Player(id),
+    FOREIGN KEY (game_id) REFERENCES GameStats(game_id),
+);
+
+CREATE TABLE TeamComparisons (
+    team1_id int NOT NULL,
+    team2_id int NOT NULL,
+    team1_win_pct float NOT NULL,
+    team2_win_pct float NOT NULL,
+    season_num int NOT NULL,
+
+
+    PRIMARY KEY (team1_id, team2_id),
+    FOREIGN KEY (team1_id) REFERENCES Team(team_id),
+    FOREIGN KEY (team2_id) REFERENCES Team(team_id),
+);
+
+CREATE TABLE PlayerComparisons (
+    player1_id int NOT NULL,
+    player2_id int NOT NULL,
+    player1_points int NOT NULL,
+    player2_points int NOT NULL,
+    season_num int NOT NULL,
+
+
+
+    PRIMARY KEY (player1_id, player2_id),
+    FOREIGN KEY (player1_id) REFERENCES Player(id),
+    FOREIGN KEY (player2_id) REFERENCES Player(id),
 );
 -- Player(id, first_name, last_name)
 -- PlayerAttributes(id, position, height_feet, height_inches, weight_pounds, team_id)
