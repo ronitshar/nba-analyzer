1c81f2ba5d93b36489559df8736771927e507a1f
liteshkurella <lakurell@purdue.edu>
2022-10-31 22:21:13 -0400

    Player Comparison History Page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var fetch = require("node-fetch");
 const db = require("../database");
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // var options = {
   //   uri: "https://www.balldontlie.io/api/v1/players/237",
   //   method: "GET",
@@ -15,10 +15,10 @@ router.get('/', async (req, res, next) => {
   const result = await db.promise().query("SELECT * FROM example");
   const rows = result[0];
   console.log(rows);
-  res.render('index', { players: rows });
+  res.render("index", { players: rows });
 });
 
-router.get('/viewcomparisons', async (req, res, next) => {
+router.get("/viewcomparisons", async (req, res, next) => {
   // var options = {
   //   uri: "https://www.balldontlie.io/api/v1/players/237",
   //   method: "GET",
@@ -26,10 +26,11 @@ router.get('/viewcomparisons', async (req, res, next) => {
   // };
   // const apiRes = await fetch("https://www.balldontlie.io/api/v1/players/237");
   // const apiResJson = await apiRes.json();
-  const result = await db.promise().query("SELECT * FROM PlayerComparisons pc JOIN Player p1 ON  pc.player1_id == t1.id JOIN Team t2 ON tc.team2_id == t2.id");
+  const result = await db.promise().query(`SELECT * 
+                                          FROM PlayerComparisons pc JOIN Player p1 ON  pc.player1_id == p1.id JOIN Player p2 ON pc.player2_id == p2.id`);
   const rows = result[0];
   console.log(rows);
-  res.render('viewcomparisons', { comparisons: rows });
+  res.render("viewcomparisons", { comparisons: rows });
 });
 
 module.exports = router;
